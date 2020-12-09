5dfc439e3ae919c00b897f3a373cfdaa310e2773
Rohan Verma <rv725555@gmail.com>
2020-12-09 13:57:59 +0530

    (improv) Unified car tables

--- Car_db_create.py
+++ Car_db_create.py
@@ -9,47 +9,41 @@ cursor.execute('CREATE DATABASE ' + settings.db_name)
 cursor.execute('USE ' + settings.db_name)
 db_connection.commit()
 
-cursor.execute('''CREATE TABLE Saloon(Serial_number int PRIMARY KEY
+cursor.execute('''CREATE TABLE ALL_CARS(Serial_number int PRIMARY KEY
 , Name varchar(30), Body_type varchar(20),  Price int, Units int)''')
 cursor.execute('''
-INSERT INTO Saloon
+INSERT INTO ALL_CARS
 VALUES (1, \'Mercedes Maybach S650\', \'Saloon\', 55000000, 3)''')
 cursor.execute(''' 
-INSERT INTO Saloon
+INSERT INTO ALL_CARS
 VALUES (2, \'BMW M5 Competition\', \'Saloon\', 23000000, 12)''')
 cursor.execute('''
-INSERT INTO Saloon
+INSERT INTO ALL_CARS
 VALUES (3, \'Audi A8 W12 Quattro\', \'Saloon\', 37000000, 5)
 ''')
-
-cursor.execute('''CREATE TABLE SUV(Serial_number int PRIMARY KEY
-, Name varchar(30), Body_type varchar(20), Price int, Units int)''')
 cursor.execute('''
-INSERT INTO SUV
-VALUES (1, \'Lamborghini Urus\', \'SUV\', 30000000, 6) 
+INSERT INTO ALL_CARS
+VALUES (4, \'Lamborghini Urus\', \'SUV\', 30000000, 6) 
 ''')
 cursor.execute('''
-INSERT INTO SUV
-VALUES (2, \'Rolls Royce Cullinan\', \'SUV\', 70000000, 8) 
+INSERT INTO ALL_CARS
+VALUES (5, \'Rolls Royce Cullinan\', \'SUV\', 70000000, 8) 
 ''') 
 cursor.execute(''' 
-INSERT INTO SUV
-VALUES (3, \'Aston Martin DBX\', \'SUV\', 45000000, 3) 
+INSERT INTO ALL_CARS
+VALUES (6, \'Aston Martin DBX\', \'SUV\', 45000000, 3) 
 ''')
-
-cursor.execute('''CREATE TABLE Supercar(Serial_number int PRIMARY KEY
-, Name varchar(30), Body_type varchar(20), Price int, Units int)''')
 cursor.execute(''' 
-INSERT INTO Supercar
-VALUES (1, \'Koenigsiegg Regera\', \'Supercar\', 110000000, 4) 
+INSERT INTO ALL_CARS
+VALUES (7, \'Koenigsiegg Regera\', \'Supercar\', 110000000, 4) 
 ''')
 cursor.execute('''
-INSERT INTO Supercar
-VALUES (2, \'Lamborghini Essenza\', \'Supercar\', 150000000, 2)  
+INSERT INTO ALL_CARS
+VALUES (8, \'Lamborghini Essenza\', \'Supercar\', 150000000, 2)  
 ''')
 cursor.execute(''' 
-INSERT INTO Supercar
-VALUES (3, \'Porsche 911 GT4 RS\', \'Supercar\', 21000000, 7) 
+INSERT INTO ALL_CARS
+VALUES (9, \'Porsche 911 GT4 RS\', \'Supercar\', 21000000, 7) 
 ''')
 
 db_connection.commit()
--- MySQL_Python_car_project.py
+++ MySQL_Python_car_project.py
@@ -7,22 +7,32 @@ def get_db_connection():
     cursor = db_connection.cursor()
     return [db_connection, cursor]
 
+def get_car_types():
+    db_connection, cursor = get_db_connection()
+    cursor.execute('Select distinct body_type from ALL_CARS')
+    type_list = []
+    for record in cursor.fetchall():
+        type_list.append(record[0])
+    return type_list
+
 def car_reorder(t):
     db_connection, cursor = get_db_connection()
-    cursor.execute('UPDATE ' + t[2] + ' SET Units = 3 where name = \'' + t[1] + '\'')
+    cursor.execute('UPDATE ALL_CARS SET Units = 3 where name = \'' + t[1] + '\'')
     db_connection.commit()
     db_connection.close()
 
-def display_models(table_name, condition):
+def display_models(condition):
     db_connection, cursor = get_db_connection()
-    cursor.execute('select * from ' + table_name, condition)
+    cursor.execute('select * from ALL_CARS', condition)
     max_l1 = 0
     max_l2 = 0
     max_l3 = 0
-    max_l4 = 0 
+    max_l4 = 0
+    max_l5 = 0
     for model in cursor:
         name_l = len(model[1])
         serial_l = len(str(model[0]))
+        bodytype_l = len(str(model[2]))
         price_l = len(str(model[3]))
         unit_l = len(str(model[4]))
         if name_l > max_l1:
@@ -33,41 +43,26 @@ def display_models(table_name, condition):
             max_l3 = price_l
         if unit_l > max_l4:
             max_l4 = unit_l
+        if bodytype_l > max_l5:
+            max_l5 = bodytype_l
     n = 3
-    cursor.execute('select * from ' + table_name, condition)
+    cursor.execute('select * from ALL_CARS', condition)
     for model in cursor:
-        print(str(model[0]).rjust(max_l2) +  ' ' * n + model[1].ljust(max_l1) + ' ' * n + model[2] + ' ' * n + str(model[3]).rjust(max_l3) + ' ' * n + str(model[4]).rjust(max_l4) )
+        print(str(model[0]).rjust(max_l2) +  ' ' * n + model[1].ljust(max_l1) + ' ' * n + model[2].ljust(max_l5) + ' ' * n + str(model[3]).rjust(max_l3) + ' ' * n + str(model[4]).rjust(max_l4) )
     db_connection.close()
-
-def get_car_types():
-    db_connection, cursor = get_db_connection()
-    cursor.execute('show tables')
-    car_types = []
-    for x in cursor:
-        car_types.append(x[0])
-    db_connection.close()
-    return car_types
 
 def get_price_range():
     max_price = 0
     db_connection, cursor = get_db_connection()
-    cursor.execute('show tables')
-    tables = cursor.fetchall()
-    for index, table in enumerate(tables):
-        name = table[0]
-        cursor.execute('select min(price) from ' + name)
-        selected_min_price = cursor.fetchall()[0][0]
-        if index == 0 or selected_min_price < min_price:
-            min_price = selected_min_price
-        cursor.execute('select max(price) from ' + name)
-        selected_max_price = cursor.fetchall()[0][0]
-        if selected_max_price > max_price:
-            max_price = selected_max_price
+    cursor.execute('select min(price) from ALL_CARS')
+    min_price = cursor.fetchall()[0][0]
+    cursor.execute('select max(price) from ALL_CARS')
+    max_price = cursor.fetchall()[0][0]
     return [min_price, max_price]
  
-def buying_a_car(serial_number, car_type):
+def buying_a_car(serial_number):
     db_connection, cursor = get_db_connection()
-    cursor.execute('select * from ' + car_type + ' where serial_number = ' + str(serial_number))
+    cursor.execute('select * from ALL_CARS where serial_number = ' + str(serial_number))
     model_chosen = cursor.fetchall()[0] 
     print('The ' + model_chosen[1] + ' will cost you ' + str(model_chosen[3] * 1.28) +  ' after taxes')
     option = 0 
@@ -79,79 +74,63 @@ def buying_a_car(serial_number, car_type):
         if model_chosen[-1] == 0:
             print('Since this car isn\'t currently in stock, we will place an order and deliver this car to you tomorrow.')
             car_reorder(model_chosen)
-        decrease_stock(car_type, serial_number, 1)
+        decrease_stock(serial_number, 1)
     elif option == 2:
         print('Thanks for visiting us. We hope to do business with you in the near future.')
-    
-def display_car_type_list(car_types):
-    for index, car_type in enumerate(car_types):
-        print(str(index + 1) + '. ' + car_type.capitalize())
 
-def select_car_type(car_types):
-    display_car_type_list(car_types)
-    option = 0
-    while option not in range(1, len(car_types) + 1):
-        option = int(input('Enter >>> '))
-        print('')
-    selected_car_type = car_types[option - 1]
-    print('')
-    display_models(selected_car_type, '')
-    return selected_car_type
-
-def increase_stock(table_name ,serial_number, increment):
+def increase_stock(serial_number, increment):
     db_connection, cursor = get_db_connection() 
-    cursor.execute('update ' + table_name + ' SET Units = Units + ' + str(increment) + ' where Serial_number = ' + str(serial_number) + ';')
+    cursor.execute('update ALL_CARS SET Units = Units + ' + str(increment) + ' where Serial_number = ' + str(serial_number) + ';')
     db_connection.commit()
     db_connection.close()
 
-def is_serial_number_valid(table_name, serial_number):
+def is_serial_number_valid(serial_number):
     db_connection, cursor = get_db_connection() 
-    cursor.execute('select * from ' + table_name + ';')
+    cursor.execute('select * from ALL_CARS;')
     serial_list = []
     l = cursor.fetchall()
     for content in l:
         serial_list.append(content[0])
-
     if serial_number in serial_list:
         return  True
     else:
         return False
 
-def price_change(table_name, serial_number, new_price):
+def price_change(serial_number, new_price):
     db_connection, cursor = get_db_connection() 
-    cursor.execute('update ' + table_name + ' set Price = ' + str(new_price) + ' where Serial_number = ' + str(serial_number) + ';')
+    cursor.execute('update ALL_CARS set Price = ' + str(new_price) + ' where Serial_number = ' + str(serial_number) + ';')
     db_connection.commit()
     db_connection.close()
 
-def decrease_stock(table_name, serial_number, decrement):
+def decrease_stock(serial_number, decrement):
     db_connection, cursor = get_db_connection() 
-    cursor.execute('update ' + table_name + ' SET Units = Units - ' + str(decrement) + ' where Serial_number = ' + str(serial_number) + ';')
+    cursor.execute('update ALL_CARS SET Units = Units - ' + str(decrement) + ' where Serial_number = ' + str(serial_number) + ';')
     db_connection.commit()
     db_connection.close()
 
-def add_vehicle(table_name, name, body_type, price):
+def add_vehicle(name, body_type, price):
     db_connection, cursor = get_db_connection() 
-    cursor.execute('select * from ' + table_name + ';')
+    cursor.execute('select * from ALL_CARS;')
     l = cursor.fetchall()
 
     serial = l[-1][0]
-    cursor.execute('insert into ' + table_name + ' values(' + str(serial + 1) + ', ' + '\'' + name + '\'' + ', ' + '\'' +body_type + '\'' + ', ' + str(price) + ', ' + '2);')
+    cursor.execute('insert into ' + table_name + ' values(' + str(serial + 1) + ', ' + '\'' + name + '\'' + ', ' + '\'' + body_type + '\'' + ', ' + str(price) + ', ' + '2);')
     db_connection.commit()
     db_connection.close()
     return serial
 
-def remove_vehicle(table_name, serial_number):
+def remove_vehicle(serial_number):
     db_connection, cursor = get_db_connection() 
-    cursor.execute('delete from ' + table_name + ' where Serial_number = ' + str(serial_number) + ';')
+    cursor.execute('delete from ALL_CARS where Serial_number = ' + str(serial_number) + ';')
     db_connection.commit()
     db_connection.close()
 
-def check_serial_number(selected_car_type, serial_number):
-    if is_serial_number_valid(selected_car_type, serial_number) == True:
+def check_serial_number(serial_number):
+    if is_serial_number_valid(serial_number) == True:
         return serial_number
     else:
         new_serial_number = eval(input('Enter a valid serial number for ' + selected_car_type + ' --> '))
-        updated_serial_number = check_serial_number(selected_car_type, new_serial_number)
+        updated_serial_number = check_serial_number(new_serial_number)
         return updated_serial_number
 
 print('Welcome to the finest luxury car dealership in the country.')
@@ -173,84 +152,67 @@ Enter >>> '''))
 2. Exit
 Enter >>> '''))
         print('')
-        if option == 2:
-            break
-        elif option == 1:
-            option = 0
-            while option not in [1,2,3]:
-                option = int(input('''
-1. Buy according to body type
-2. Buy according to budget
-3. Exit
-Enter >>> '''))
+        if option == 1:
+            option = 0      
+            min_price, max_price = get_price_range()
+            print('')
+            print('Our cars range from ' + str(min_price/10000000) + ' Cr' + ' to ' +  str(max_price/10000000) + ' Cr')
+            budget = 'r'
+            while budget[-1].lower() != 'cr': 
+                budget = input('Please enter your max budget in the specified range in the format <1 Cr>\n\nEnter >>> ')
                 print('')
-            car_types = get_car_types()
-            if option == 1:
-                print('')
-                selected_car_type = select_car_type(car_types)
-                
-                input_serial_number = int(input('Enter >>> '))
-                serial_number = check_serial_number(selected_car_type, input_serial_number)
-                print('\n')
-                buying_a_car(option, selected_car_type)
-            elif option == 2:
-                min_price, max_price = get_price_range()
-                print('')
-                print('Our cars range from ' + str(min_price/10000000) + ' Cr' + ' to ' +  str(max_price/10000000) + ' Cr')
-                budget = 'r'
-                while budget[-1].lower() != 'cr': 
-                    budget = input('Please enter your max budget in the specified range in the format <1 Cr>\n\nEnter >>> ')
-                    print('')
-                    budget = budget.split()
-                budget = float(budget[0]) * 10000000
-                if budget < min_price:
-                    print('Sorry! No cars are available within your budget.')
-                    break
-                print('')
-                for num, car_type in enumerate(car_types):
-                    print(str(num + 1) + '. ' + car_type.upper())
-                    print('')
-                    display_models(car_type, ' where price < ' + str(budget + 1))
-                    print('')
-                input_from_user_1 = int(input('Please enter the Serial Number of the body type you want.\nEnter >>> '))
-                print('')
-                input_from_user_2 = int(input('Please enter the Index of the model you want.\nEnter >>> '))
-                print('')
-                serial_number = check_serial_number(car_types[input_from_user_1 - 1], input_from_user_2)
-                buying_a_car(serial_number, car_types[input_from_user_1 - 1])
-            elif option == 3:
+                budget = budget.split()
+            budget = float(budget[0]) * 10000000
+            if budget < min_price:
+                print('Sorry! No cars are available within your budget.')
                 break
+            print('')
+            display_models(' where price < ' + str(budget + 1))
+            print('')
+            input_from_user_1 = int(input('Please enter the Index of the model you want.\nEnter >>> '))
+            print('')
+            serial_number = check_serial_number(input_from_user_1)
+            buying_a_car(serial_number)
+        elif option == 2:
+            break
 
     elif option == 2:
         print('\n')
-        print('Car types in stock:')
-        car_types = get_car_types()
-        selected_car_type = select_car_type(car_types)
+        display_models()
         print('\n\nThese are the models in stock.')
         option = int(input('''What would you like to do?\n\n
 1. Decrease units of a vehicle
-2. Change price of any vehicle
-3. Add a new vehicle
-4. Remove a vehicle
-5. Quit
+2. Add units of a vehicle
+3. Change price of any vehicle
+4. Add a new vehicle
+5. Remove a vehicle
+6. Quit
 Enter >>> '''))
         if option == 1:
             print('\n')
             input_from_user_1 = int(input('Which model stock do you want to reduce?\nEnter >>> '))
             input_from_user_2 = int(input('How many units do you want to reduce?\nEnter >>> '))
-            serial_number = check_serial_number(selected_car_type, input_from_user_1)
-            decrease_stock(selected_car_type, serial_number, input_from_user_2)
+            serial_number = check_serial_number(input_from_user_1)
+            decrease_stock(serial_number, input_from_user_2)
             print('Your changes have been made.')
 
         if option == 2:
             print('\n')
+            input_from_user_1 = int(input('Which model stock do you want to increase?\nEnter >>> '))
+            input_from_user_2 = int(input('How many units do you want to add?\nEnter >>> '))
+            serial_number = check_serial_number(input_from_user_1)
+            decrease_stock(serial_number, input_from_user_2)
+            print('Your changes have been made.')
+            
+        if option == 3:
+            print('\n')
             input_from_user_1 = int(input('Which model price do you want to change?\nEnter >>> '))
             input_from_user_2 = int(input('Please enter the new price.\nEnter >>> '))
-            serial_number = check_serial_number(selected_car_type, input_from_user_1)
-            price_change(selected_car_type, serial_number, input_from_user_2)
+            serial_number = check_serial_number(input_from_user_1)
+            price_change(serial_number, input_from_user_2)
             print('Your changes have been made.')
 
-        if option == 3:
+        if option == 4:
             print('\n')
             name = str(input('Please enter the car name. \n Enter >>> '))
             l1 = name.split()
@@ -260,20 +222,26 @@ Enter >>> '''))
             price = 0
             while price <= 0 or price >= 1000000000:
                 price = int(input('Please enter price of the vehicle less than 100 Crores. \nEnter >>> '))
-            vehicle = [name, selected_car_type, price]
-            add_vehicle(selected_car_type, vehicle[0], vehicle[1], vehicle[2])
+            car_types = get_car_types()
+            for index, type in enumerate(car_types()):
+                print(str(index + 1) + '. ' + type)
+            type_input = -1
+            while type_input not in range(1, 4):
+                type_input = int(input('Please choose the type of the car you are adding. \nEnter >>> '))
+            vehicle = [name, type_input, price]
+            add_vehicle(vehicle[0], vehicle[1], vehicle[2])
             print('New vehicle has been added. Order for addition of units has been sent to the supplier which we will recieve in a few days.')
             vehicle = [''] + vehicle
             car_reorder(vehicle)
 
-        if option == 4:
+        if option == 5:
             print('\n')
             input_serial_number = int(input('Enter serial number of vehicle to be removed >>> '))
-            serial_number = check_serial_number(selected_car_type, input_serial_number)
-            remove_vehicle(selected_car_type, serial_number)
+            serial_number = check_serial_number(input_serial_number)
+            remove_vehicle(serial_number)
             print('Changes Saved.')
 
-        if option == 5:
+        if option == 6:
             break
     
     elif option == 3:
